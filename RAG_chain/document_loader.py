cb167dd981b56e0d66c8f626951761e5ef487d51
crankyastrologer <ansh21csu519@ncuindia.edu>
2024-07-29 14:06:19 +0530

    final comment

--- RAG_chain/document_loader.py
+++ RAG_chain/document_loader.py
@@ -1,4 +1,4 @@
-from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 
@@ -9,8 +9,9 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 def document_loader():
-    loader = DirectoryLoader("new_data", glob="*.txt")
+    loader = CSVLoader('data/data.csv')
     docs = loader.load()
+    print(docs)
     docs_texts = [d.page_content for d in docs]
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = text_splitter.split_documents(docs)
